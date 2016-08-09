2aa4fd3212a479345c55c3df1829cdefb6f4cfa7
felixacdc <pfmata01@gmail.com>
2016-08-09 01:22:48 -0600

    [add] backend register people

--- app/app/Http/Controllers/RegisterController.php
+++ app/app/Http/Controllers/RegisterController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Http\Requests\PeopleRequest;
 use App\Http\Controllers\Controller;
 
 use App\Models\Code;
+use App\Models\People;
 
 class RegisterController extends Controller
 {
@@ -28,9 +30,46 @@ class RegisterController extends Controller
         }
     }
 
-    public function store(Request $request)
+    public function store(PeopleRequest $request)
     {
-        dd($request);
+        try {
+            if ( $this->verify_save_code($request->code) ) {
+                $code_id = Code::where('code', $request->code)->firstOrFail();
+
+                $person = new People;
+                $person->name = $request->name;
+                $person->email = $request->email;
+                $person->phone = $request->phone;
+                $person->semester = $request->semester;
+                $person->plan = $request->plan;
+                $person->course = $request->course;
+                $person->code_id = $code_id->id;
+
+                $person->save();
+
+                $code_id->state = true;
+                $code_id->save();
+
+                return redirect('/')->with('success', 'Registrado correctamente');
+            } else {
+                throw new \Exception('Error');
+            }
+        } catch (\Exception $e) {
+            return redirect('/')->with('error', 'No se pudo realizar el registro');
+        }
+
+
+    }
+
+    private function verify_save_code($code)
+    {
+        $number = Code::where('code', $code)->where('state', false)->count();
+
+        if ( $number > 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
---
+++ app/app/Http/Requests/PeopleRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class PeopleRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'      => 'required',
+            'email'     => 'required|email',
+            'phone'     => 'required|min:8',
+            'semester'  => 'required|in:Primero,Segundo,Tercero,Cuarto,Quinto,Sexto,Septimo,Octabo,Noveno,Decimo,Onceavo,Dosceavo',
+            'plan'      => 'required|in:Diario,Fin de Semana',
+            'course'    => 'required',
+            'code'      => 'required'
+        ];
+    }
+}
