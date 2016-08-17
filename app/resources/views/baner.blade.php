4e6d262c8a736b2cc2849a073b35e5656e95443f
felixacdc <pfmata01@gmail.com>
2016-08-16 21:32:57 -0600

    [modify] banner

--- app/resources/views/baner.blade.php
+++ app/resources/views/baner.blade.php
@@ -1,12 +1,6 @@
 <div id="index-banner" class="parallax-container">
     <div class="section no-pad-bot">
         <div class="container">
-            <br><br>
-            <h1 class="header center default_color_text wow zoomInDown">Universidad Mariano Gálvez</h1>
-            <div class="row center">
-                <a href="#register" class="btn-large waves-effect waves-light red wow flipInX">Registrate</a>
-            </div>
-            <br><br>
         </div>
     </div>
 </div>
